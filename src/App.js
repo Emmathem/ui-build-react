9cb8e326dc9258720e59bb38167106b02dda402c
Falua Temitope Oyewole <emmat0616@gmail.com>
2021-04-14 00:02:22 +0100

    Header nav

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./assets/css/main.css";
+import AppHeader from "./components/AppHeader";
+import AppLogin from "./components/AppLogin";
 import AppRegister from "./components/AppRegister";
 import AppWelcome from "./components/AppWelcome";
 
@@ -7,10 +10,12 @@ const App = () => {
   return (
     <div>
       <Router>
+        <AppHeader />
         <div>
           <Switch>
-            <Route exact path="/" component={AppWelcome} />
+            <Route path="/" component={AppWelcome} />
             <Route path="/register" component={AppRegister} />
+            <Route path="/login" component={AppLogin} />
           </Switch>
         </div>
       </Router>
