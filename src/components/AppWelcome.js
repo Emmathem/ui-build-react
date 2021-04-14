d22a67331654b80fc8bc14ed49dc660e7fbc188b
Falua Temitope Oyewole <emmat0616@gmail.com>
2021-04-14 01:48:39 +0100

    Welcome Landing

--- src/components/AppWelcome.js
+++ src/components/AppWelcome.js
@@ -6,6 +6,10 @@ const AppWelcome = () => {
     <div className="landing">
       <div className="landing__bgCover">
         <div className="landing__welcome">
+          <div className="landing__content">
+            <h3>Want to Meet A</h3>
+            <h4>Desperate Escort?</h4>
+          </div>
           <Link to="/register">
             <button type="button" className="landing__getStartedBtn">
               Get Started
