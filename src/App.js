a7132202ee7c503e0fde2575d08816ec37be9ce0
Falua Temitope Oyewole <emmat0616@gmail.com>
2021-04-14 11:16:48 +0100

    Animation Doord

--- src/App.js
+++ src/App.js
@@ -2,25 +2,42 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./assets/css/main.css";
 import "antd/dist/antd.css";
-// import "./assets/css/animate.css";
-import AppHeader from "./components/AppHeader";
+// import AppHeader from "./components/AppHeader";
 import AppLogin from "./components/AppLogin";
 import AppRegister from "./components/AppRegister";
 import AppWelcome from "./components/AppWelcome";
+import AppLandingPage from "./components/AppLandingPage";
 
 const App = () => {
 
+  // return (
+  //   <div className="landing">
+  //     <Router>
+  //       <AppHeader />
+  //       <div className="wow animated fadeIn landing__bgCover">
+  //         <Switch>
+  //           <Route path="/first" component={AppLandingPage} />
+  //           <Route exact path="/" component={AppWelcome} />
+  //           <Route path="/register" component={AppRegister} />
+  //           <Route path="/login" component={AppLogin} />
+  //         </Switch>
+  //       </div>
+  //     </Router>
+  //   </div>
+  // );
+  // const [hover, setIsHover] = 
+
+
   return (
-    <div className="landing">
+    <div className="">
       <Router>
-        <AppHeader />
-        <div className="wow animated fadeIn landing__bgCover">
-          <Switch>
+        {/* <AppHeader /> */}
+        <Switch>
+            <Route path="/first" component={AppLandingPage} />
             <Route exact path="/" component={AppWelcome} />
             <Route path="/register" component={AppRegister} />
             <Route path="/login" component={AppLogin} />
           </Switch>
-        </div>
       </Router>
     </div>
   );
