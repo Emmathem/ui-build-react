159ba416a2e973e35c7fd0716a2cdd0ece456ad9
Falua Temitope Oyewole <emmat0616@gmail.com>
2021-04-16 13:55:52 +0100

    Lang added

--- src/components/call_of_duty/AppCallOfDutyExtended.js
+++ src/components/call_of_duty/AppCallOfDutyExtended.js
@@ -1,16 +1,26 @@
-import React from 'react'
-import { Helmet } from 'react-helmet'
-import AppCODHeader from './AppCODHeader'
+import React from "react";
+import { Helmet } from "react-helmet";
+import AppCODHeader from "./AppCODHeader";
 import "../../assets/css/cod.css";
 
 const AppCallOfDutyExtended = () => {
-    return (
-        <div>
-            <Helmet><title>COD | Call of Duty Extended</title></Helmet>
-            <AppCODHeader />
-            <div className="codExtended"></div>
-        </div>
-    )
-}
+  return (
+    <div>
+      <Helmet>
+        <title>COD | Call of Duty Extended</title>
+      </Helmet>
+      <AppCODHeader />
+      <div className="codExtended">
+          <div className="codExtended__lang">
+              <div className="codExtended__lang__inner">
+                  <div><span>DE</span></div>
+                  <div><span>FR</span></div>
+                  <div className="active"><span>EN</span></div>
+              </div>
+          </div>
+      </div>
+    </div>
+  );
+};
 
-export default AppCallOfDutyExtended
+export default AppCallOfDutyExtended;
