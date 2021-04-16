3328669983ddff4cd97e11de9672608f2328ebc4
Falua Temitope Oyewole <emmat0616@gmail.com>
2021-04-16 16:34:35 +0100

    SLide

--- src/components/call_of_duty/AppCallOfDutyExtended.js
+++ src/components/call_of_duty/AppCallOfDutyExtended.js
@@ -4,6 +4,10 @@ import AppCODHeader from "./AppCODHeader";
 import "../../assets/css/cod.css";
 import ComputerIcon from "@material-ui/icons/Computer";
 import { Button } from "antd";
+import { AutoRotatingCarousel } from "material-auto-rotating-carousel";
+import NewCode from "../../assets/images/the-new-cod.png";
+import Slide from "@material-ui/core/Slide";
+// const Slide = require('./Slide').default;
 
 const AppCallOfDutyExtended = () => {
   return (
@@ -34,17 +38,39 @@ const AppCallOfDutyExtended = () => {
                 <h2 className="codColor">CALL OF DUTY</h2>
                 <h3>EXTENDED</h3>
                 <p className="codExtended__content__span1">
-                  <ComputerIcon style={{ marginRight: '16px'}} /> PC REQUIREMENTS
+                  <ComputerIcon style={{ marginRight: "16px" }} /> PC
+                  REQUIREMENTS
                 </p>
                 <p className="codExtended__content__span1">
                   SEASON 2 AVAILABLE FROM
                   <span>&nbsp; MARCH 1, 2021</span>
                 </p>
-                <Button className="codExtended__content__button">Sign up Now</Button>
+                <Button className="codExtended__content__button">
+                  Sign up Now
+                </Button>
               </div>
             </div>
             <div className="codExtended__inner__col">
-              <div className="codExtended__slideshow">SLide</div>
+              <div className="codExtended__slideshow">
+                <h2>Slide</h2>
+                <AutoRotatingCarousel label='Get started'>
+                  <Slide
+                    media={<img src={NewCode} alt="slide1" />}
+                    title="This is a very cool feature"
+                    subtitle="Just using this will blow your mind."
+                  />
+                  <Slide
+                    media={<img src={NewCode} alt="slide1" />}
+                    title="Ever wanted to be popular?"
+                    subtitle="Well just mix two colors and your are good to go!"
+                  />
+                  <Slide
+                    media={<img src={NewCode} alt="slide1" />}
+                    title="May the force be with you"
+                    subtitle="The Force is a metaphysical and ubiquitous power in the Star Wars fictional universe."
+                  />
+                </AutoRotatingCarousel>
+              </div>
             </div>
           </div>
         </div>
