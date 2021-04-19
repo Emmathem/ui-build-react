f5d00473930bc922af98692114fb8268cd7d82ae
Falua Temitope Oyewole <emmat0616@gmail.com>
2021-04-19 10:04:39 +0100

    UI Project

---
+++ src/components/mover_ng/AppMoverHeader.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AppMoverHeader = () => {
+    return (
+        <div className="moverheader">
+            
+        </div>
+    )
+}
+
+export default AppMoverHeader
